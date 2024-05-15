b1f143b1bf7968378b93e70caf92674b6885a6cc
sajedaalrababah <sajedaalrababah@icloud.com>
2024-05-15 21:32:50 +0300

    2

--- src/Component/Productlist.js
+++ src/Component/Productlist.js
@@ -1,73 +1,63 @@
 import React, { useState, useEffect } from "react";
- 
 import { Link } from "react-router-dom";
 import axios from "axios";
- 
-function Productlist()
-{ 
-    const[product, setProduct]= useState([]);
-     
-    useEffect( ()=>{
-        const getProduct= ()=>{
+
+function Productlist() {
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        const getProduct = () => {
             fetch("http://127.0.0.1:8000/api/products")
-            .then(res=>{ return res.json()})
-            .then(response=>{ 
-                console.log(response.products)
-                setProduct(response.products)
-            })
-            .catch(error=>{ console.log(error)});
-        }
+                .then(res => res.json())
+                .then(response => {
+                    console.log(response.products);
+                    setProduct(response.products);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        };
         getProduct();
-    },[]);
-  
-   
+    }, []);
+
     const deleteProduct = (id) => {
-        axios.delete('http://127.0.0.1:8000/api/productdelete/'+id).then(function(response){
+        axios.delete('http://127.0.0.1:8000/api/productdelete/' + id).then(function (response) {
             console.log(response.data);
             alert("Successfully Deleted");
+            setProduct(product.filter(p => p.id !== id));
+        }).catch(error => {
+            console.log(error);
         });
-    }
-     
-    return(
+    };
+
+    return (
         <React.Fragment>
             <div className="container container_overflow">
                 <div className="row">
                     <div className="col-12">
-                        <h5 className="mb-4">Product List</h5> 
-                        <p className="text-danger"> </p>                 
-                                <table className="table table-bordered">
-                                <thead>
-                                <tr>
-                                <th scope="col">Sr.No</th>
-                                <th scope="col">Product Title</th>
-                                <th scope="col">Product Description</th>
-                                <th scope="col">Product Image</th>
-                                <th scope="col" width="200">Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        product.map((pdata, index)=>(
-                                            <tr key={index}>
-                                            <td>{index+1 } </td>
-                                            <td>{pdata.name } </td>
-                                            <td>{pdata.description } </td>
-                                            <td><img src={`http://127.0.0.1:8000/storage/${pdata.image}`} alt="" height={50} width={90} /></td>
-                                            <td>
+                        <h5 className="mb-4">Product List</h5>
+                        <div className="row">
+                            {
+                                product.map((pdata, index) => (
+                                    <div key={index} className="col-md-4 mb-4">
+                                        <div className="card">
+                                            <img src={`http://127.0.0.1:8000/storage/${pdata.image}`} className="card-img-top" alt={pdata.name} height={200} style={{ objectFit: 'cover' }} />
+                                            <div className="card-body">
+                                                <h5 className="card-title">{pdata.name}</h5>
+                                                <p className="card-text">{pdata.description}</p>
                                                 <Link to={`/editproduct/${pdata.id}/edit`} className="btn btn-success mx-2">Edit</Link>
                                                 <button onClick={() => deleteProduct(pdata.id)} className="btn btn-danger">Delete</button>
-                                            </td>
-                                            </tr>
-                                        ))
-                                    }
-                               
-                                                                
-                                </tbody>
-                                </table>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
         </React.Fragment>
     );
 }
+
 export default Productlist;
